0385a63a8d1aeeec5f6a46d8b7f5391bd49a3cb0
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-18 20:53:49 +0200

    .

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import katex from 'katex'
+  import { onMount } from 'svelte';
+  var input = "\\sqrt[3]{\\frac{2^7-2^x}{2^x-2}}=2"
+  $:kt = katex.renderToString(input, {output: 'mathml'})
+</script>
+
+<main>
+  <input type="text" bind:value={input}>
+  <div>
+    {@html kt}
+  </div>
+</main>
+
+<style>
+:global(.katex) {
+  font-size: 60px;
+}
+</style>
